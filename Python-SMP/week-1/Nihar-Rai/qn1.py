8318372e3544225d7c6af5694f5ab03e42c74dbb
Niharrai <50584781+Niharrai@users.noreply.github.com>
2019-05-14 03:21:43 +0530

    Assignment 1

--- Python-SMP/week-1/Nihar-Rai/qn1.py
+++ Python-SMP/week-1/Nihar-Rai/qn1.py
@@ -2,13 +2,17 @@ list1 = ['I','S','T','E']
 list2 = list1 #list2 points at same object as 1
 list2[0] ='1'#change in 0 position changes list1 too since same object is being used by both lists
 print(list1)
-list1 = list1 + []
+list1 = list1 + []#item is added 
 list2[1] = 2
-print(list1)#not sure why there is no change
+print(list1)
+#Output1 ['1','S','T','E']
+#output2 ['1','S','T','E']
 list1 = ['I','S','T','E']
 list2 = list1# same as above
 list2[0] = '1'
 print(list1)
 list1+=[]# blank space is added to left of list1
 list2[1] = 2
-print(list1)#object list1 and 2 point to are still same so all changes in list2 are observed
+print(list1)#object list1 and 2 point to, are still same so all changes in list2are observed
+#output1 ['1','S','T','E']
+#output2 ['1',2,'T','E']
