0321fe1b4670970067ccbbe48375191a7fe6a8ac
maheshbabugorava <maheshbabugorava007@gmail.com>
2019-05-13 21:56:34 +0530

    assignment 1

--- Python-SMP/Week-1/mahesh/q2.py
+++ Python-SMP/Week-1/mahesh/q2.py
@@ -6,3 +6,16 @@ for i in range(n):
             a[i],a[j]= a[j],a[i]
 print(a)
 
+k = 10
+j = 0
+for i in a:
+    if i == k :
+        print("3 is found")
+        break
+    else :
+        j +=1
+
+if j == len(a) :
+    print("3 is not found")
+
+
